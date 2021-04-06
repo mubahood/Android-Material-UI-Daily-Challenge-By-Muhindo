5217409adfa2c64b34c141cdcf6aac6382b3c1dc
Muhindo Mubaraka <30492534+iut-cse-4301-mubahood@users.noreply.github.com>
2021-04-06 06:13:30 +0600

    first

--- app/src/main/java/com/muhindo/android_material_ui_daily_challenge/activity/card/CardBasicActivity.java
+++ app/src/main/java/com/muhindo/android_material_ui_daily_challenge/activity/card/CardBasicActivity.java
@@ -14,6 +14,7 @@ public class CardBasicActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_basic);
+        //this is a simple comment
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
